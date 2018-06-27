d7d7953a052819817c0b83173d68425e649256c3
Tim Shaker <tmshkr@users.noreply.github.com>
2018-06-27 22:21:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-function shout(string) {
+function shout(string){
     return string.toUpperCase();
 }
 
-function whisper(string) {
+function whisper(string){
     return string.toLowerCase();
 }
+
+function logShout(string){
+    console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+    console.log(string.toLowerCase());
+}
+
